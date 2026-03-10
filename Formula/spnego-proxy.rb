af26e403ebf5209e8ddf7ffda6be208642cc83bb
Andrew Sweet <andrew.sweet@cantab.net>
2026-03-10 21:08:53 +0000

    feat: add spnego-proxy formula for v1.0.0

---
+++ Formula/spnego-proxy.rb
@@ -0,0 +1,22 @@
+class SpnegoProxy < Formula
+  desc "SPNEGO/Kerberos authenticating HTTP proxy"
+  homepage "https://github.com/andrewesweet/spnego-proxy"
+  url "https://github.com/andrewesweet/spnego-proxy/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "de8b43d26d3c8b4cb3c1ca9df3b0123d484f3288448807fba1b6f2840a046b9a"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=HEAD
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spnego-proxy -version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-spnego-proxy
+
+Homebrew formula for [spnego-proxy](https://github.com/andrewesweet/spnego-proxy),
+a SPNEGO/Kerberos authenticating HTTP proxy.
+
+## Install
+
+```sh
+brew tap andrewesweet/spnego-proxy
+brew install spnego-proxy
+```
+
+## Update
+
+```sh
+brew update
+brew upgrade spnego-proxy
+```
